9aac1732341a14f4991762b7cfcba701686b81a9
Antonio <jimenezc.bo@gmail.com>
2026-02-02 23:36:42 -0600

    added sigma points mean cov recovery

--- src/estimate.py
+++ src/estimate.py
@@ -5,7 +5,7 @@ class EstimateUKF: # cite paper
         '''UKF hyperparameters'''
         self.alpha = alpha # 10e-2 < alpha < 1
         self.kappa = kappa # belongs to {0, 3-L}
-        self.beta = beta # = 2 for Gaussina RV
+        self.beta = beta # = 2 for Gaussian RV
         # self.jitter = 1e-12 ?
 
     def symmetrize(self, matrix):
@@ -20,11 +20,40 @@ class EstimateUKF: # cite paper
         sqrt_cov = np.linalg.cholesky(cov, upper=False)
         sigma_points = np.empty((2*L + 1, L)) # points stored in rows
         sigma_points[0] = mean
-        # Traspose covariance and rely on numpy casting
+        # Traspose covariance and rely on numpy broadcasting
         sigma_points[1:L+1] = mean + gamma * sqrt_cov.T
         sigma_points[L+1:2*L+1] = mean - gamma * sqrt_cov.T
 
         return sigma_points
+    
+    def ukf_mean_cov(self, sigma_points):
+        L = sigma_points.shape[1]
+        lam = self.alpha**2 * (L + self.kappa) - L
+        
+        alpha_0_m = lam / (L + lam)
+        alpha_k_m = 0.5 / (L + lam)
+
+        # Calculate mean (rely on numpy broadcasting)
+        weights_mean = np.zeros((2*L + 1))
+        weights_mean[0] = alpha_0_m
+        weights_mean[1:] = alpha_k_m
+        mean = np.sum(weights_mean[:,None] * sigma_points, axis = 0)
+
+        alpha_0_c = alpha_0_m + (1 - self.alpha**2 + self.beta)
+        alpha_k_c = alpha_k_m
+        weights_cov = np.zeros((2*L + 1))
+        weights_cov[0] = alpha_0_c
+        weights_cov[1:] = alpha_k_c
+        sigma_points_centered = sigma_points - mean # mean Numpy broadcasting (L,) -> (2L+1, L), then diff.
+        # Also relying on numpy broadcasting rules to implement weighted sum of outer products
+        outer_product = sigma_points_centered[:,:,None] * sigma_points_centered[:,None,:]
+        cov = np.sum((weights_cov[:,None,None] * outer_product), axis=0)
+        cov = self.symmetrize(cov)
+        
+        return mean, cov
+    
+
+
     
 
 
